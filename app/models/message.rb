cff2f6f626732ee9796e5a53b56456cefdd54c3b
Devendra Asus <devendrap@shriffle.com>
2024-04-16 10:31:01 +0530

    change sender to receiver in model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
-  belongs_to :receiver, class_name: 'User', foreign_key: 'sender_id'
+  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
 
   before_validation :check_self_message
 
