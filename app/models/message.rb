9829244fde44abb860851b2e79944ae6c64396c0
Devendra Asus <devendrap@shriffle.com>
2024-04-16 09:38:33 +0530

    self message disble

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'sender_id'
+
+  before_validation :check_self_message
+
+  private
+
+  def check_self_message
+    if sender.id == receiver.id
+      errors.add(:base, "can't send message to self")
+    end
+  end
 end
